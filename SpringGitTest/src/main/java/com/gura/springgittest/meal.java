ecdec705a887c7c3d07f5e0d5ce5b508bb8955ad
user <user@user-PC>
2019-06-03 16:48:47 +0900

    interface class controller update

---
+++ SpringGitTest/src/main/java/com/gura/springgittest/meal.java
@@ -0,0 +1,8 @@
+package com.gura.springgittest;
+
+public interface meal {
+
+	public String breakfast();
+	
+	public String lunch(); 
+}
---
+++ SpringGitTest/src/main/java/com/gura/springgittest/mealImpl.java
@@ -0,0 +1,17 @@
+package com.gura.springgittest;
+
+public class mealImpl implements meal {
+
+	@Override
+	public String breakfast() {
+		
+		return null;
+	}
+
+	@Override
+	public String lunch() {
+		
+		return null;
+	}
+
+}
